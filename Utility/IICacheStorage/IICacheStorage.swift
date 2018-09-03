a4d54e4fb78cd48e0fa0b169968529970604910b
Noah_Shan <shanwzh@inspur.com>
2018-09-03 17:50:06 +0800

    = 缓存添加 =

---
+++ Utility/IICacheStorage/IICacheStorage.swift
@@ -0,0 +1,34 @@
+//
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * ** * * * *
+//
+// IICacheStorage.swift
+//
+// Created by    Noah Shan on 2018/6/15
+// InspurEmail   shanwzh@inspur.com
+//
+// Copyright © 2018年 Inspur. All rights reserved.
+//
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * ** * * * *
+
+
+import Foundation
+
+/// 用来存储库名字，key名字
+class IICacheStorage : NSObject {
+    
+    /// 缓存库名称
+    @objc lazy var iiCacheStorageName = "impCloud+CacheStorage"
+    
+    /// 个人中心-tablist数据源缓存key
+    @objc lazy var cloudMinePersonCenterTabList = "cloudMinePersonCenterTabList"
+    
+    /// 发现-卡包信息
+    @objc lazy var discoveryCardsList = "discoveryCardsList"
+    
+    /// 预制的note添加
+    @objc lazy var preNoteInsert = "preNoteInsert"
+    
+    override init() {
+        super.init()
+    }
+}
