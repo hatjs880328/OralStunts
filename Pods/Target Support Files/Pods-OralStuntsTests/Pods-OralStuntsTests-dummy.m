3d444da7cc2bd0d35d5f5d35ddfa392e6ed31f9a
Noah_Shan <shanwzh@inspur.com>
2018-11-12 11:31:23 +0800

    ====Noah Shan====
    忽略pod文件夹
    
    ====END====

--- Pods/Target Support Files/Pods-OralStuntsTests/Pods-OralStuntsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OralStuntsTests : NSObject
-@end
-@implementation PodsDummy_Pods_OralStuntsTests
-@end
--- Pods/Target Support Files/WHDebugTool/WHDebugTool-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_WHDebugTool : NSObject
-@end
-@implementation PodsDummy_WHDebugTool
-@end
